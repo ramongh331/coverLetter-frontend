5f1e59c1361d7a50e246f535977f27853a95a7b3
Ramon Hernandez <ramongh331@gmail.com>
2023-02-22 20:06:40 -0500

    home page is structure and responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,29 +5,56 @@ import HomeImg from '../images/happy-news.svg'
 export default function Home() {
   return (
     <>
-      <main className="bg-blue-1000 w-full h-screen flex flex-col items-center ">
+      <main className="bg-blue-1000 
+      w-full 
+      h-screen
+      flex flex-col items-center ">
         <Header />
-        <section className="w-2/5 max-sm:w-4/5 h-full flex flex-col items-center justify-evenly ">
-          <h2 className="text-5xl max-sm:text-2xl text-white font-bold leading-[4rem] ">
+        <section className="max-sm:w-4/5 sm:w-3/5 xl:w-2/5 w-2/5
+        h-full">
+          <h2 className=" max-[280px]:text-2xl max-sm:text-3xl sm:text-4xl text-5xl
+          text-white 
+          font-bold 
+          leading-[4rem] 
+          mt-10">
             Get noticed by employers with our professional cover letters
           </h2>
-          <p className="text-3xl max-sm:text-lg text-white leading-10">
+          <p className="max-[280px]:text-sm max-sm:text-lg sm:text-xl text-3xl
+          text-white 
+          leading-10
+          mt-5">
             Make your application process easy and stress-free by creating a
             cover letter that showcases your skills and experience.
           </p>
-          <section className="w-2/3 max-sm:w-11/12 flex justify-evenly">
+          <section className="max-[280px]:w-full max-sm:max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg
+          flex justify-evenly 
+          max-sm:mx-auto max-sm:mt-7 sm:mx-auto sm:mt-7">
+
             <Link to="/coverletters/new">
-              <div className="bg-orange-1000 text-white w-40 max-sm:w-24 h-11 max-sm:h-8 text-2xl max-sm:text-sm flex justify-center items-center rounded-full font-semibold">
+              <div className="bg-orange-1000 text-white 
+              w-40 max-sm:w-24 sm:w-32 
+              h-11 max-sm:h-8 sm:h-9 
+              text-2xl max-sm:text-sm sm:text-base 
+              flex justify-center items-center 
+              rounded-full 
+              font-semibold">
                 Get Started
               </div>
             </Link>
             <Link to="/coverletters">
-              <div className="bg-white w-40 max-sm:w-24 h-11 max-sm:h-8 text-2xl max-sm:text-sm flex justify-center items-center rounded-full font-semibold">
+              <div className="bg-white 
+              w-40 max-sm:w-24 sm:w-32 
+              h-11 max-sm:h-8 sm:h-9 
+              text-2xl max-sm:text-sm sm:text-base 
+              flex justify-center items-center 
+              rounded-full 
+              font-semibold">
                 Examples
               </div>
             </Link>
           </section>
-          <img className="max-sm:shrink" src={HomeImg} alt=""/>
+          <img className="max-sm:w-56 sm:w-80 
+          max-sm:mx-auto max-sm:mt-7 sm:mx-auto sm:mt-7" src={HomeImg} alt=""/>
         </section>
       </main>
     </>

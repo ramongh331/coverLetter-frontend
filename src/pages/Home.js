9887d42836a54a7261aa77ef01d3f56ffac25c61
Ramon Hernandez <ramongh331@gmail.com>
2023-02-23 22:09:26 -0500

    new page styling done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,53 +7,53 @@ export default function Home() {
       <main className="bg-blue-1000 
       w-full 
       h-screen
-      flex flex-col items-center ">
-        <section className="max-sm:w-4/5 sm:w-3/5 xl:w-2/5 w-2/5
-        h-full">
-          <h2 className=" max-[280px]:text-2xl max-sm:text-3xl sm:text-4xl text-5xl
-          text-white 
-          font-bold 
-          leading-[4rem] 
-          mt-10">
-            Get noticed by employers with our professional cover letters
-          </h2>
-          <p className="max-[280px]:text-sm max-sm:text-lg sm:text-xl text-3xl
-          text-white 
-          leading-10
-          mt-5">
-            Make your application process easy and stress-free by creating a
-            cover letter that showcases your skills and experience.
-          </p>
-          <section className="max-[280px]:w-full max-sm:max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg
-          flex justify-evenly 
-          max-sm:mx-auto max-sm:mt-7 sm:mx-auto sm:mt-7">
-
-            <Link to="/coverletters/new">
-              <div className="bg-orange-1000 text-white 
-              w-40 max-sm:w-24 sm:w-32 
-              h-11 max-sm:h-8 sm:h-9 
-              text-2xl max-sm:text-sm sm:text-base 
-              flex justify-center items-center 
-              rounded-full 
-              font-semibold">
-                Get Started
-              </div>
-            </Link>
-            <Link to="/coverletters">
-              <div className="bg-white 
-              w-40 max-sm:w-24 sm:w-32 
-              h-11 max-sm:h-8 sm:h-9 
-              text-2xl max-sm:text-sm sm:text-base 
-              flex justify-center items-center 
-              rounded-full 
-              font-semibold">
-                Examples
-              </div>
-            </Link>
+      flex flex-col items-center 
+      ">
+          <section className="max-sm:w-4/5 sm:w-3/5 xl:w-2/5 w-2/5
+          h-full">
+            <h2 className=" max-[280px]:text-2xl max-sm:text-3xl sm:text-4xl text-5xl
+            text-white
+            font-bold
+            leading-[4rem]
+            mt-10">
+              Get noticed by employers with our professional cover letters
+            </h2>
+            <p className="max-[280px]:text-sm max-sm:text-lg sm:text-xl text-3xl
+            text-white
+            leading-10
+            mt-5">
+              Make your application process easy and stress-free by creating a
+              cover letter that showcases your skills and experience.
+            </p>
+            <section className="max-[280px]:w-full max-sm:max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg
+            flex justify-evenly
+            max-sm:mx-auto max-sm:mt-7 sm:mx-auto sm:mt-7">
+              <Link to="/coverletters/new">
+                <div className="bg-orange-1000 text-white
+                w-40 max-sm:w-24 sm:w-32
+                h-11 max-sm:h-8 sm:h-9
+                text-2xl max-sm:text-sm sm:text-base
+                flex justify-center items-center
+                rounded-full
+                font-semibold">
+                  Get Started
+                </div>
+              </Link>
+              <Link to="/coverletters">
+                <div className="bg-white
+                w-40 max-sm:w-24 sm:w-32
+                h-11 max-sm:h-8 sm:h-9
+                text-2xl max-sm:text-sm sm:text-base
+                flex justify-center items-center
+                rounded-full
+                font-semibold">
+                  Examples
+                </div>
+              </Link>
+            </section>
+            <img className="max-sm:w-56 sm:w-80
+            max-sm:mx-auto max-sm:mt-7 sm:mx-auto sm:mt-7" src={HomeImg} alt=""/>
           </section>
-          <img className="max-sm:w-56 sm:w-80 
-          max-sm:mx-auto max-sm:mt-7 sm:mx-auto sm:mt-7" src={HomeImg} alt=""/>
-        </section>
       </main>
     </>
   );
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,30 +1,29 @@
 import { Form } from "react-router-dom";
 import "../index.css";
-import { useState } from "react";
 
 export default function New() {
 
-    const [showInput, setShowInput] = useState(false)
-
   return (
     <>
-      <main className="bg-blue-1000 w-full h-full ">
+      <main className="w-full h-full ">
         <section className="flex flex-col items-center">
             <h2 className="max-[280px]:text-3xl max-sm:text-4xl text-5xl 
             font-bold
             text-white 
             mt-5
             mx-3">Make a New Cover Letter</h2>
-            <div className="flex max-[280px]:flex-col 
+            <div className="max-[280px]:flex-col max-sm:flex-col max-[875px]:flex-col flex
             justify-evenly 
-            max-[280px]:items-center
+            max-[280px]:items-center max-sm:items-center max-[875px]:items-center
             w-full 
             h-4/5 
             max-[280px]:mt-0 mt-10">
+              
+              {/* Form */}
               <Form
                 className="flex flex-col 
-                items-center 
-                max-[280px]:w-11/12 w-96"
+                items-center
+                max-[280px]:w-11/12 max-sm:w-11/12 w-96"
                 action="/coverletters/create"
                 method="post"
               >
@@ -32,75 +31,83 @@ export default function New() {
                 <section className="flex flex-col 
                 justify-evenly 
                 items-center 
-                max-[280px]:w-full w-3/5 
+                max-[280px]:w-full max-sm:w-full sm:w-full w-3/5 
                 h-[40rem]">
                   <input
-                    className="w-full h-8 rounded-md p-1"
+                    className="max-sm:max-w-[18rem] sm:max-w-[18rem] w-full 
+                    h-8 
+                    rounded-md 
+                    p-1"
                     type="text"
                     name="name"
                     placeholder="Your Name"
                   />
                   <input
-                    className="w-full h-8 rounded-md p-1"
+                    className="max-sm:max-w-[18rem] sm:max-w-[18rem] w-full h-8 rounded-md p-1"
                     type="text"
                     name="position"
                     placeholder="Job Position"
                   />
                   <input
-                    className="w-full h-8 rounded-md p-1"
+                    className="max-sm:max-w-[18rem] sm:max-w-[18rem] w-full h-8 rounded-md p-1"
                     type="text"
                     name="date"
                     placeholder="Today's Date"
                   />
                   <input
-                    className="w-full h-8 rounded-md p-1"
+                    className="max-sm:max-w-[18rem] sm:max-w-[18rem] w-full h-8 rounded-md p-1"
                     type="text"
                     name="company"
                     placeholder="Company's Name"
                   />
                   <input
-                    className="w-full h-8 rounded-md p-1"
+                    className="max-sm:max-w-[18rem] sm:max-w-[18rem] w-full h-8 rounded-md p-1"
                     type="text"
                     name="skill"
                     placeholder="Relevant Skills/Experience"
                   />
                   <input
-                    className="w-full h-8 rounded-md p-1"
+                    className="max-sm:max-w-[18rem] sm:max-w-[18rem] w-full h-8 rounded-md p-1"
                     type="text"
                     name="yoe"
                     placeholder="Years of Experience"
                   />
                   <input
-                    className="w-full h-8 rounded-md p-1"
+                    className="max-sm:max-w-[18rem] sm:max-w-[18rem] w-full h-8 rounded-md p-1"
                     type="text"
                     name="expertise"
                     placeholder="Relevant Expertise"
                   />
                   <input
-                    className="w-full h-8 rounded-md p-1"
+                    className="max-sm:max-w-[18rem] sm:max-w-[18rem] w-full h-8 rounded-md p-1"
                     type="text"
                     name="passion"
                     placeholder="Relevant Field/Industry"
                   />
                   <input
-                    className="w-full h-8 rounded-md p-1"
+                    className="max-sm:max-w-[18rem] sm:max-w-[18rem] w-full h-8 rounded-md p-1"
                     type="text"
                     name="products"
                     placeholder="Company's Products/Services"
                   />
-                  <button className="bg-orange-1000 w-11/12 font-bold text-white text-md rounded-lg py-1">Create a Cover Letter</button>
+                  <button className="bg-orange-1000 
+                  max-sm:max-w-[13rem] sm:max-w-[13rem] w-11/12 
+                  font-bold 
+                  text-white text-md 
+                  rounded-lg 
+                  py-1">Create a Cover Letter</button>
                 </section>
               </Form>
               {/* Example Paper */}
               <section className="bg-white 
-              max-[280px]:w-11/12 w-[40rem] 
+              max-[280px]:w-11/12 max-sm:w-11/12 min-[430px]:max-w-[26rem]  w-[40rem] 
               text-left 
-              max-[280px]:text-sm text-xl 
+              max-[280px]:text-sm max-sm:text-sm sm:text-base text-xl 
               p-5 
               flex flex-col 
               justify-between 
               shadow-lg
-              max-[280px]:mb-3">
+              max-[280px]:mb-3 max-sm:mb-6 sm:mb-6 mb-6">
                 <p>
                   Dear <span className="redText">[Hiring Manager]</span>,
                 </p>

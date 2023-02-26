9496e3671a8d528cc43c01d1fe3cd898376232d3
Ramon Hernandez <ramongh331@gmail.com>
2023-02-25 21:41:10 -0500

    styled examples page

--- src/pages/Examples.js
+++ src/pages/Examples.js
@@ -1,5 +1,6 @@
 import { useLoaderData, Link } from "react-router-dom";
 import { useState } from "react";
+import './Examples.css'
 
 export default function Examples() {
   const letters = useLoaderData();
@@ -16,7 +17,7 @@ transform: `translateX(-${imgsInView * show}%)`
 }
 
 function handleNextSlide(){
-  if(imgsInView < (length - 2)){
+  if(imgsInView < (length)){
      setImgsInView(prevState => prevState + 2)
   }
 }
@@ -31,42 +32,54 @@ function handleBackSlide(){
       <main className=" h-screen">
           <Link className="bg-purple-1000 
           absolute 
-          min-[280px]:w-10 2xl:w-16 
-          min-[280px]:h-7 2xl:h-16 
+          min-[280px]:w-10 sm:w-14  
+          min-[280px]:h-10 sm:h-14  
           rounded-full 
-          min-[280px]:top-[20.1rem] sm:top-[22rem]
+          min-[280px]:top-[19.8rem] sm:top-[22.2rem]
           min-[280px]:left-[50%]
-          2xl:right-28 
-          2xl:bottom-28 
-          2xl:flex 
-          2xl:justify-center 
-          2xl:items-start" 
+          min-[280px]:translate-x-[-50%]
+          flex 
+          justify-center
+          items-start" 
           to="/coverletters/new">
-            <button className=" text-white 
-            max-[280px]:mt-0 2xl:mt-[5px] 
-            max-[280px]:text-base 2xl:text-5xl 
+            <button className=" text-white  
+            min-[280px]:text-3xl sm:text-5xl 
             font-bold">
               +</button>
           </Link>
           {/* Under Header */}
           <section className="bg-purple-50 
           w-full 
-          h-72 
-          min-[280px]:flex 2xl:flex
-          min-[280px]:flex-col 
-          min-[280px]:justify-center 2xl:justify-evenly 
-          items-center 
+          underHeader
           overflow-hidden ">
-              <h2 className="min-[280px]:text-2xl 2xl:text-5xl
-              ">Look at all your well written Cover Letters!</h2>
-              <img className="min-[280px]:h-48 2xl:h-96
-               mt-7" src="https://i.imgur.com/jMqSXdI.png" alt="two purple pencils in the shape of the letters C and L"/>
+              <h2 className="underHeaderText">Look at all your well written Cover Letters!</h2>
+              <img className="clImg" src="https://i.imgur.com/jMqSXdI.png" alt="two purple pencils in the shape of the letters C and L"/>
           </section>
-          <section className="overflow-hidden relative h-80 mt-10">
-              <section style={moveStyle} className="w-fit h-full flex items-center ease-in-out duration-300">
+          <section className="overflow-hidden relative 
+          h-80 max-2xl:h-fit 
+          mt-10">
+              <section style={moveStyle} className="w-fit 
+              h-full 
+              flex
+              max-2xl:flex-wrap
+              max-2xl:justify-center 
+              items-center 
+              ease-in-out 
+              duration-300
+              ">
                   {letters.map((letter) => (
-                    <Link className="w-80 h-52 mx-5 " to={`/coverletters/${letter.id}/`}>
-                      <section className="bg-[#e8d7ff] box-border flex flex-col justify-between w-full h-60 text-left rounded-xl shadow-lg">
+                    <Link className="w-80 max-sm:w-72 
+                    h-60 
+                    mx-5 " to={`/coverletters/${letter.id}/`}>
+                      <section className="bg-[#e8d7ff] 
+                      box-border 
+                      flex flex-col 
+                      justify-between 
+                      w-full sm:
+                      max-sm:h-48 sm:h-52 
+                      text-left 
+                      rounded-xl 
+                      shadow-lg">
                         <p className="p-2">
                           I am writing to express my interest in the {letter.position} role
                           at {letter.company}. I am excited to apply my skills in{" "}
@@ -80,11 +93,11 @@ function handleBackSlide(){
                       </Link>
               ))}
               </section>
-              <div className="bg-white opacity-50 absolute w-28 h-full right-0 top-0"></div>
-              <div className="absolute text-9xl right-2 top-24 cursor-pointer" onClick={handleNextSlide}> &gt; </div>
+              <div className="bg-white opacity-50 absolute w-28 h-full right-0 top-0 max-2xl:hidden"></div>
+              <div className="absolute text-9xl right-2 top-24 cursor-pointer max-2xl:hidden" onClick={handleNextSlide}> &gt; </div>
             {imgsInView === 0 ? "" : <>
-            <div className="bg-white opacity-50 absolute w-28 h-full top-0"></div>
-            <div className="absolute text-9xl left-2 top-24 cursor-pointer" onClick={handleBackSlide}>&lt;</div>
+            <div className="bg-white opacity-50 absolute w-28 h-full top-0 max-2xl:hidden"></div>
+            <div className="absolute text-9xl left-2 top-24 cursor-pointer max-2xl:hidden" onClick={handleBackSlide}>&lt;</div>
             </>}      
           </section>
       </main>
